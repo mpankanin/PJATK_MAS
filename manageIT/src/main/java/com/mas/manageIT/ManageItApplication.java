13e506b99ae25ed151d338cb2be62ca1fec4a2ed
s24188 <s24188@pjwstk.edu.pl>
2024-04-09 00:02:55 +0200

    Initial commit - manageIT

---
+++ manageIT/src/main/java/com/mas/manageIT/ManageItApplication.java
@@ -0,0 +1,13 @@
+package com.mas.manageIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageItApplication.class, args);
+	}
+
+}
---
+++ manageIT/src/test/java/com/mas/manageIT/ManageItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.manageIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
