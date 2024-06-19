fbe65802d8a32455cf45a7d59d0020c5f065e09e
s24188 <s24188@pjwstk.edu.pl>
2024-06-19 22:26:12 +0200

    Final commit

--- manageIT/manageit/src/main/java/com/mas/manageIT/model/Document.java
+++ manageIT/manageit/src/main/java/com/mas/manageIT/model/Document.java
@@ -1,6 +1,7 @@
 package com.mas.manageIT.model;
 
 import com.mas.manageIT.associacion_manager.ObjectPlusPlus;
+import com.mas.manageIT.mapper.OrderMapper;
 import com.mas.manageIT.model.enums.DocumentType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,5 +20,12 @@ public class Document extends ObjectPlusPlus {
     private DocumentType type;
 
     private String description;
+
+
+    public static Document createDocument(long id, DocumentType type, String description, Order order) {
+        Document document = new Document(id, type, description);
+        document.addLink("DocumentOrder", "OrderDocument", order);
+        return new Document(id, type, description);
+    }
 
 }
--- manageIT/manageit/src/main/java/com/mas/manageIT/service/implementation/DocumentServiceImpl.java
+++ manageIT/manageit/src/main/java/com/mas/manageIT/service/implementation/DocumentServiceImpl.java
@@ -1,17 +1,48 @@
 package com.mas.manageIT.service.implementation;
 
+import com.mas.manageIT.associacion_manager.ObjectPlus;
+import com.mas.manageIT.exception.OrderNotFoundException;
+import com.mas.manageIT.model.Document;
+import com.mas.manageIT.model.Order;
 import com.mas.manageIT.repository.DocumentRepository;
 import com.mas.manageIT.service.DocumentService;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DocumentServiceImpl implements DocumentService {
 
     private final DocumentRepository documentRepository;
+    private final Logger logger = LoggerFactory.getLogger(DocumentServiceImpl.class);
 
 
     public DocumentServiceImpl(DocumentRepository documentRepository) {
         this.documentRepository = documentRepository;
+    }
+
+    @Override
+    public Document add(Document document, long orderId) {
+        try {
+            List<Order> extent = (List<Order>) ObjectPlus.getExtent(Order.class);
+            Optional<Order> optional = extent.stream().filter(o -> o.getId().equals(orderId)).findFirst();
+
+            List<Document> documentExtent = (List<Document>) ObjectPlus.getExtent(Document.class);
+
+            if (optional.isPresent()) {
+                documentExtent.remove(document);
+                Document createdDocument = Document.createDocument(documentExtent.size() + 1, document.getType(), document.getDescription(), optional.get());
+                return createdDocument;
+            } else {
+                throw new OrderNotFoundException("Order: " + orderId + " doesn't exist.");
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error(() -> "Couldn't get a order's extent.");
+        }
+        return null;
     }
 
 }
